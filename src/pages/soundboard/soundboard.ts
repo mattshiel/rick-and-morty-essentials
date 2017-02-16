596684279cae8b75cd6782df1bd4f8a278ad57dc
Matthew Shiel <Matthew Shiel>
2017-02-16 01:14:36 +0000

    Themeing the app

--- src/pages/soundboard/soundboard.ts
+++ src/pages/soundboard/soundboard.ts
@@ -10,63 +10,40 @@ import { MediaPlugin } from 'ionic-native';
 
 export class SoundboardPage {
 
-  media: MediaPlugin = new MediaPlugin('/android_asset/www/soundfiles/lick_my_balls.mp3');
+media: MediaPlugin = new MediaPlugin('/android_asset/www/soundfiles/lick_my_balls.mp3');
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-
-  }
-
-  ionViewDidEnter() {
-    this.media = new MediaPlugin('/android_asset/www/soundfiles/lick_my_balls.mp3');
-  }
-
-  showAlert(message) 
-  {
-  let alert = this.alertCtrl.create({
-    title: 'Error',
-    subTitle: message,
-    buttons: ['OK']
-  });
-  alert.present();
-}
-
-  startRecording() 
-  {
-  try {
-    this.media.startRecord();
-  }
-  catch (e) {
-    this.showAlert('Could not start recording.');
-  }
-}
-
-stopRecording() 
+constructor(public navCtrl: NavController, public alertCtrl: AlertController) 
 {
-  try {
-    this.media.stopRecord();
-  }
-  catch (e) {
-    this.showAlert('Could not stop recording.');
-  }
+
 }
 
-startPlayback() {
-  try {
+ionViewDidEnter() 
+{
+  this.media = new MediaPlugin('/android_asset/www/soundfiles/lick_my_balls.mp3');
+}
+
+showAlert(message) 
+{
+  let alert = this.alertCtrl.create({
+  title: 'Error',
+  subTitle: message,
+  buttons: ['OK']
+});
+
+alert.present();
+}
+
+startPlayback() 
+{
+  try 
+  {
     this.media.play();
   }
-  catch (e) {
+
+  catch (e) 
+  {
     this.showAlert('Could not play recording.');
   }
-}
-
-stopPlayback() {
-  try {
-    this.media.stop();
-  }
-  catch (e) {
-    this.showAlert('Could not stop playing recording.');
-  }
-
 }
 
 }
